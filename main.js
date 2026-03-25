b7a5c9295e7e230e13a001f276b69d833ea02edb
gavinhousley <gavinhousley@gmail.com>
2026-03-25 23:20:55 +0000

    add splash page links from images, and some JS for code page fade-in

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ burger.addEventListener("click", () => {
   nav.classList.toggle("open");
   burger.setAttribute("aria-expanded", nav.classList.contains("open"));
 });
+
+// Fade in for code.html
+
+if (document.body.id === "code") {
+  document.body.style.backgroundColor = "d9d9d9";
+
+  document.addEventListener("DOMContentLoaded", () => {
+    document.body.style.transition = "background-color 1.5s";
+    document.body.style.backgroundColor = "black";
+  });
+}
