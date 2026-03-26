56b007064389ddb2024ab4d17433a984e964bbda
gavinhousley <gavinhousley@gmail.com>
2026-03-26 23:05:19 +0000

    add links from images on the landing page, and re-sorted code page with some ascii art. Nav to fix.

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ burger.addEventListener("click", () => {
   nav.classList.toggle("open");
   burger.setAttribute("aria-expanded", nav.classList.contains("open"));
 });
+
+document.querySelector("#code-link").addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("nav").style.color = "white";
+  document.body.style.transition = "background-color 2s";
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+});
